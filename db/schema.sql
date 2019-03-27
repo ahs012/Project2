7701e1e1889b1bc71f3de4d793c9b2a193cebf1d
Smoss <ahs012@gmail.com>
2019-03-26 20:34:32 -0400

    DB Set-Up

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,21 @@
 DROP DATABASE IF EXISTS users_db;
 -- Creates the "blogger" database --
 CREATE DATABASE users_db;
+USE users_db;
+CREATE TABLE users(
+id INT PRIMARY KEY,
+type VARCHAR(100),
+name VARCHAR(100),
+user_name VARCHAR(100),
+location VARCHAR(100),
+email VARCHAR(100)
+);
+
+CREATE TABLE art_table(
+ art_id INT PRIMARY KEY AUTO_INCREMENT,
+ users_id INT,
+ FOREIGN KEY (users_id) REFERENCES users(id),
+ title VARCHAR(100),
+ art_medium VARCHAR(100),
+ released DATE 
+ );
